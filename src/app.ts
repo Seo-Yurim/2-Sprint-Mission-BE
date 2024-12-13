a0fb67b3f5796a549ba6d0864d7d7c8dc1e69a2c
Seo-Yurim <yurim2204@naver.com>
2024-12-13 11:13:12 +0900

    chore: 패키지 설치 및 수정

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import productRoutes from './routes/productRoutes.js';
-import articleRoutes from './routes/articleRoutes.js';
-import { handleErrors } from './utils/globalErrorHandler.js';
-import requestLogger from './utils/requestLogger.js';
-import responseLogger from './utils/responseLogger.js';
+import productRoutes from './routes/productRoutes';
+import articleRoutes from './routes/articleRoutes';
+import { handleErrors } from './utils/globalErrorHandler';
+import requestLogger from './utils/requestLogger';
+import responseLogger from './utils/responseLogger';
 
 dotenv.config();
 const app = express();
